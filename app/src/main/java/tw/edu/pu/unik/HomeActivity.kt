3092a03d8ed0175ec307729e29efe6c710e12711
林儒靖 <style410211@gmail.com>
2021-10-27 14:15:03 +0800

    first commit

---
+++ app/src/main/java/tw/edu/pu/unik/HomeActivity.kt
@@ -0,0 +1,26 @@
+package tw.edu.pu.unik
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import androidx.navigation.findNavController
+import androidx.navigation.fragment.NavHostFragment
+import androidx.navigation.ui.setupWithNavController
+import com.google.android.material.bottomnavigation.BottomNavigationItemView
+import com.google.android.material.bottomnavigation.BottomNavigationView
+
+class HomeActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_home)
+
+        bottomNavigationSetup()
+
+    }
+    fun bottomNavigationSetup() {
+        val bottomNavigationView = findViewById<BottomNavigationView>(R.id.bottomNavigationView)
+        val navHostFragment = supportFragmentManager.findFragmentById(R.id.fragmentContainerView) as NavHostFragment
+        val navController = navHostFragment.navController
+        bottomNavigationView.setupWithNavController(navController)
+
+    }
+}
